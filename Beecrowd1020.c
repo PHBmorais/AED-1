ed9c15ce7a87c13a97ea292e640d8446be327428
PHBmorais <phbarbosa077@gmail.com>
2024-10-16 17:14:48 -0300

    Add files via upload

---
+++ Beecrowd1020.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+ 
+// Converter a idade de uma pessoa em dias em anos, meses e dias
+int main() {
+    
+    int idade, anos, meses,dias;
+    
+    scanf("%d", &idade);
+       
+    // Descobrir os anos
+    anos = idade / 365;
+    // Pegar o resto da divisão por ano
+    idade = idade % 365;
+    // Descobrir os meses
+    meses = idade / 30;
+    // Pegar o resto da divisão por mês e descobrir dias
+    dias = idade % 30;
+       
+    printf("%d ano(s)\n", anos);
+    printf("%d mes(es)\n", meses);
+    printf("%d dia(s)\n", dias);
+    
+    return 0;
+   }
